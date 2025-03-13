6f58c3133b75c2db5b38073f1a63bbbcba296116
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-13 14:33:34 -0700

    Add additional routes

--- routes/temple.js
+++ routes/temple.js
@@ -1,9 +1,23 @@
 const routes = require('express').Router();
 const temples = require('../controllers/temple.js');
 
+// Find all temples
 routes.get('/', temples.findAll);
+
+// Find Single Temple
 routes.get('/:temple_id', temples.findOne);
 
+// Create a Temple entry
 routes.post('/', temples.create);
+
+// Update a Temple with id
+routes.put('/:id', temples.update);
+
+// Delete a Temple with id
+routes.delete('/:id', temples.delete);
+
+// Create a new Temple
+routes.delete('/', temples.deleteAll);
+
 
 module.exports = routes;
